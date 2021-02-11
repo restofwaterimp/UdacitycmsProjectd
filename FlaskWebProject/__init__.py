8fd13c7f9ac77860a1a2bd4a17ae315f77d571b4
restofwaterimp <tmkz.it@gmail.com>
2021-02-11 11:03:13 +0900

    change logginglevel warn to error

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,9 +12,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 wsgi_app = app.wsgi_app
-app.logger.setLevel(logging.WARNING)
+app.logger.setLevel(logging.ERROR)
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.WARNING)
+streamHandler.setLevel(logging.ERROR)
 app.logger.addHandler(streamHandler)
 Session(app)
 db = SQLAlchemy(app)
