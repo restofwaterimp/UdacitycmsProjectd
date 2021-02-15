d93d49302a8b2f7d89cd8f043ce0dfe864e77207
restofwaterimp <tmkz.it@gmail.com>
2021-02-15 19:54:20 +0900

    chnage method argument

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -102,7 +102,7 @@ def authorized():
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         app.logger.info("before msal")
-        #result = _build_msal_app(cache=cache, authority=app.config.get('AUTHORITY')).acquire_token_by_authorization_code(request.args['code'],scopes=app.config.get('SCOPE'),redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+        result = _build_msal_app(cache=cache, authority=app.config.get('AUTHORITY')).acquire_token_by_authorization_code(request.args['code'],scopes=app.config.get('SCOPE'),redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         #result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'],scopes=Config.SCOPE,redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         app.logger.info("after msal")
         #result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
@@ -149,10 +149,10 @@ def _save_cache(cache):
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
     app.logger.info("build msam app")
-    # return msal.ConfidentialClientApplication(
-    #     app.config.get('CLIENT_ID'),authority=authority or app.config.get('AUTHORITY'),
-    #     client_credential=Config.CLIENT_SECRET,token_cache=cache)
-    return None
+    return msal.ConfidentialClientApplication(
+        app.config.get('CLIENT_ID'),authority=authority or app.config.get('AUTHORITY'),
+        client_credential=Config.CLIENT_SECRET,token_cache=cache)
+    # return None
 
 # def _build_auth_code_flow(authority=None, scopes=None):
 #     return _build_msal_app(authority=authority).initiate_auth_code_flow(
@@ -162,8 +162,8 @@ def _build_msal_app(cache=None, authority=None):
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
     app.logger.info("build auth url")
-    # return _build_msal_app(None,authority=authority).get_authorization_request_url(
-    #     scopes or [],
-    #     state=state or str(uuid.uuid4()),
-    #     redirect_uri=url_for('authorized', _external=True, _schema='https'))
-    return None
+    return _build_msal_app(app.config.get('CLIENT_ID'),authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for('authorized', _external=True, _schema='https'))
+    # return None
