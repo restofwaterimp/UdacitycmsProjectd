e7f8ebdfc23d4dd2a8086704a674e55877557c88
restofwaterimp <tmkz.it@gmail.com>
2021-02-15 22:02:00 +0900

    add cache

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -167,7 +167,7 @@ def _build_msal_app(cache=None, authority=None):
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
     app.logger.info("build auth url")
-    return _build_msal_app(None,authority=authority).get_authorization_request_url(
+    return _build_msal_app(_load_cache(),authority=authority).get_authorization_request_url(
         scopes or [],
         state=state or str(uuid.uuid4()),
         redirect_uri=url_for('authorized', _external=True, _schema='https'))
