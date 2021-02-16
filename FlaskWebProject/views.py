7e17b7b6437ef62a85d1ced9d848fa987d8581a8
restofwaterimp <tmkz.it@gmail.com>
2021-02-16 20:26:09 +0900

    edit error page is comment out

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -113,7 +113,7 @@ def authorized():
         #result = _build_msal_app(cache=cache).acquire_token_by_auth_code_flow(
         #   session.get("flow", {}), request.args)
         #result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'],scopes=Config.SCOPE,redirect_uri=url_for('login', _external=True, _scheme='https'))
-        result = {'error': 'Not Implemented', 'error_description': 'Function not implemented.'}
+        #result = {'error': 'Not Implemented', 'error_description': 'Function not implemented.'}
         if "error" in result:
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
