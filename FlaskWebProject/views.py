49c364699ba98db1841385625774cc1aa12540f3
restofwaterimp <tmkz.it@gmail.com>
2021-02-16 20:41:28 +0900

    token cache

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -102,7 +102,7 @@ def authorized():
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
         app.logger.info("before msal")
-        token = _build_msal_app(None, authority=Config.AUTHORITY)
+        token = _build_msal_app(cache=cache, authority=Config.AUTHORITY)
         result = token.acquire_token_by_authorization_code(request.args['code'],scopes=app.config.get('SCOPE'),redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         #result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'],scopes=Config.SCOPE,redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         # result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
@@ -168,7 +168,7 @@ def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
     app.logger.info("build auth url")
     cache = _load_cache()
-    return _build_msal_app(cache=cache,authority=authority).get_authorization_request_url(
+    return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state or str(uuid.uuid4()),
         redirect_uri=url_for('authorized', _external=True, _schema='https'))
